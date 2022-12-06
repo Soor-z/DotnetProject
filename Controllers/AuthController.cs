3b338479c1407ee574a84d08dd5968c8b178b7da
Soor-z <hsuraj608@gmail.com>
2022-12-07 00:30:02 +0545

    Initial Commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CameraShop.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Signup()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CameraShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Camera()
+        {
+            return View();
+        }
+        public IActionResult Lenses()
+        {
+            return View();
+        }
+        public IActionResult Tripod()
+        {
+            return View();
+        }
+        public IActionResult Stablizer()
+        {
+            return View();
+        }
+        public IActionResult VideoGear()
+        {
+            return View();
+        }
+    }
+}
